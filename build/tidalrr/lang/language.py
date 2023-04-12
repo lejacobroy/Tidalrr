a121a732712af882eba21803a13cb856a36607eb
Jacob <lejacobroy@gmail.com>
2023-04-12 09:28:52 -0400

    fix missing language.py

---
+++ build/tidalrr/lang/language.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   language.py
+@Time    :   2020/08/19
+@Author  :   Yaronzz
+@Version :   1.0
+@Contact :   yaronhuang@foxmail.com
+@Desc    :   
+'''
+
+from tidal_dl.lang.english import LangEnglish
+
+_ALL_LANGUAGE_ = [
+    ['English', LangEnglish()]
+]
+
+class Language(object):
+    def __init__(self) -> None:
+        self.select = LangEnglish()
+
+    def __toInt__(self, str):
+        try:
+            return int(str)
+        except:
+            return 0
+    
+    def setLang(self, index):
+        index = self.__toInt__(index)
+        if index >= 0 and index < len(_ALL_LANGUAGE_):
+            self.select = _ALL_LANGUAGE_[index][1]
+        else:
+            self.select = LangEnglish()
+
+    def getLangName(self, index):
+        index = self.__toInt__(index)
+        if index >= 0 and index < len(_ALL_LANGUAGE_):
+            return _ALL_LANGUAGE_[index][0]
+        return ""
+
+    def getLangChoicePrint(self):
+        array = []
+        index = 0
+        while True:
+            name = self.getLangName(index)
+            if name == "":
+                break
+            array.append('\'' + str(index) + '\'-' + name)
+            index += 1
+        return ','.join(array)
+
+
+LANG = Language()
