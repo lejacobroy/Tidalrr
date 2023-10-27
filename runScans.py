32e4f494732f57d6826084d49af7e9f95ca288e9
Jacob <lejacobroy@gmail.com>
2023-10-27 10:24:15 -0400

    Schedule scans during the night

--- runScans.py
+++ runScans.py
@@ -8,6 +8,10 @@
 @Contact :   lejacobroy@gmail.com
 @Desc    :   
 '''
+
+import multiprocessing
+import schedule
+import time
 from tidalrr.workers import tidalrrStart
 from tidalrr.workers.scanQueuedArtists import scanQueuedArtists
 from tidalrr.workers.scanQueuedAlbums import scanQueuedAlbums
@@ -17,13 +21,38 @@ from tidalrr.workers.scanQueuedPlaylists import scanQueuedPlaylists
 def main():
     tidalrrStart()
     print('tidalrrStart')
-    #scanQueuedArtists()
+    scanQueuedArtists()
     print('Done scanning artists')
-    #scanQueuedAlbums()
+    scanQueuedAlbums()
     print('Done scanning albums')
     scanQueuedTracks()
     print('scanQueuedTracks')
     scanQueuedPlaylists()
     print('scanQueuedPlaylists')
     
-main()
+def startDownloads():
+    # Start foo as a process
+    p = multiprocessing.Process(target=main, name="downloads", args=())
+    p.start()
+
+    # Wait a maximum of 10 seconds for foo
+    # Usage: join([timeout in seconds])
+    hours = 6
+    seconds = hours * 60 * 60
+    p.join(seconds)
+
+    # If thread is active
+    if p.is_alive():
+        print("download is running... let's kill it...")
+        # Terminate foo
+        p.terminate()
+        # Cleanup
+        p.join()
+
+def mainSchedule():
+    schedule.every().day.at("23:00").do(startDownloads)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+mainSchedule()
