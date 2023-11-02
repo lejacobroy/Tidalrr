6b67a2caea9c13b862029e98a5d422778f3b0b4a
Jacob <lejacobroy@gmail.com>
2023-11-02 15:05:04 -0400

    fix db path

--- tidalrr/definitions.py
+++ tidalrr/definitions.py
@@ -1,7 +1,7 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # This is your Project Root
-DB_PATH = os.path.join(ROOT_DIR, '../database/database.db')
-SCHEMA_PATH = os.path.join(ROOT_DIR, '../database/schema.sql')
+DB_PATH = os.path.join(ROOT_DIR, '../config/database.db')
+SCHEMA_PATH = os.path.join(ROOT_DIR, '../config/schema.sql')
 URLS_TXT_PATH = os.path.join(ROOT_DIR, '../import/urls.txt')
 print(DB_PATH)
