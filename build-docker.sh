1a08b9b69d7f27822d8f570c6ab1c6a8bc30a0b8
Jacob <lejacobroy@gmail.com>
2023-11-02 14:17:10 -0400

    push docker

--- build-docker.sh
+++ build-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker buildx create --use --platform=linux/arm64,linux/amd64 --name multi-platform-builder
 docker buildx inspect --bootstrap
-docker buildx build --platform=linux/arm64,linux/amd64 --push --tag jacobroyquebec/tidalrr -f ./dockerfile .
+docker buildx build --platform=linux/arm64,linux/amd64 --push --tag jacobroyquebec/tidalrr -f ./Dockerfile .
