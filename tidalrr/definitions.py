4e0ad756e7212e0b0d32a32787fe4928c3188ef1
Jacob <lejacobroy@gmail.com>
2023-11-02 15:03:54 -0400

    fix definitions

--- tidalrr/definitions.py
+++ tidalrr/definitions.py
@@ -1,6 +1,7 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # This is your Project Root
-DB_PATH = os.path.join(ROOT_DIR, 'database/database.db')
-SCHEMA_PATH = os.path.join(ROOT_DIR, 'database/schema.sql')
-URLS_TXT_PATH = os.path.join(ROOT_DIR, 'import/urls.txt')
+DB_PATH = os.path.join(ROOT_DIR, '../database/database.db')
+SCHEMA_PATH = os.path.join(ROOT_DIR, '../database/schema.sql')
+URLS_TXT_PATH = os.path.join(ROOT_DIR, '../import/urls.txt')
+print(DB_PATH)
