ae6e5d1580991d2255e842a11c9a1766aa4aefe8
Jacob <lejacobroy@gmail.com>
2024-05-03 17:20:19 -0400

    refactor runExportPlex

--- runExportPlex.py
+++ runExportPlex.py
@@ -21,33 +21,53 @@ from tidalrr.workers import print_elapsed_time
 def make_connection(baseurl: str, token: str):
     return PlexServer(baseurl, token)
 
-def forkImportPlex():
-    # Start foo as a process
-    print(strftime("%Y-%m-%d %H:%M:%S", gmtime())+" Starting startImportPlex")
-    p = multiprocessing.Process(target=startImportPlex)
-    p.start()
-
-    p.join()
-
-    # If thread is active
-    if p.is_alive():
-        print(strftime("%Y-%m-%d %H:%M:%S", gmtime())+" ImportPlex are running... let's kill it...")
-        # Terminate foo
-        p.terminate()
-        # Cleanup
-        p.join()
-
-if __name__ == '__main__':
-    forkImportPlex()
-    #main()
-    #mainSchedule()
-
 def create_playlist(plex, audio, playlist:Playlist, tracks:list):
     plexPlaylist = plex.createPlaylist(playlist.title, section=audio.key, items=tracks)
     playlist.plexUUID = plexPlaylist.guid
     updateTidalPlaylist(playlist)
     print("Created playlist " + playlist.title)
 
+def get_matching_track(plex, search_term, library_id, filename, strip_parens=False):
+    try:
+        results = plex.search(query=strip_appenders(search_term, strip_parens), mediatype='track', sectionId=library_id)
+        if len(results) == 1:
+            return results[0]
+        else:
+            for result in results:
+                for medium in result.media:
+                    for part in medium.parts:
+                        if part.file == filename:
+                            return result
+    except plexapi.exceptions.PlexApiException as e:
+        print(e)
+        return None
+
+def strip_appenders(full_title: str, strip_parens=False):
+    no_prefix = strip_prefix(full_title)
+    return strip_suffix(no_prefix, strip_parens)
+
+
+def strip_prefix(full_title: str):
+    match = re.split(r"^[0-9]*\s-", full_title)
+    return match[0]
+
+def strip_suffix(full_title: str, strip_parens=False):
+    stripped_title = full_title
+    start_bracket = stripped_title.find('[')
+    if start_bracket > -1:
+        end_bracket = stripped_title.find(']', start_bracket)
+        if end_bracket > -1:
+            stripped_title = stripped_title[0:start_bracket].strip()
+    featuring = stripped_title.lower().find('feat.')
+    if featuring > -1:
+        stripped_title = stripped_title[0:featuring].strip()
+    if strip_parens:
+        left_paren = stripped_title.find('(')
+        if left_paren > -1:
+            right_paren = stripped_title.find(')', left_paren)
+            if right_paren > -1:
+                stripped_title = stripped_title[0:left_paren].strip()
+    return stripped_title
 
 def search_plex_for_tracks(plex, audio, playlist:Playlist):
     #f = open(m3u_file, "r")
@@ -87,51 +107,6 @@ def search_plex_for_tracks(plex, audio, playlist:Playlist):
                 print('DEBUG: Skipping ' + l)
     updateTidalPlaylistTracksPlexUUID(playlist.uuid)
     return items
-
-
-def get_matching_track(plex, search_term, library_id, filename, strip_parens=False):
-    try:
-        results = plex.search(query=strip_appenders(search_term, strip_parens), mediatype='track', sectionId=library_id)
-        if len(results) == 1:
-            return results[0]
-        else:
-            for result in results:
-                for medium in result.media:
-                    for part in medium.parts:
-                        if part.file == filename:
-                            return result
-    except plexapi.exceptions.PlexApiException as e:
-        print(e)
-        return None
-
-
-def strip_appenders(full_title: str, strip_parens=False):
-    no_prefix = strip_prefix(full_title)
-    return strip_suffix(no_prefix, strip_parens)
-
-
-def strip_prefix(full_title: str):
-    match = re.split(r"^[0-9]*\s-", full_title)
-    return match[0]
-
-
-def strip_suffix(full_title: str, strip_parens=False):
-    stripped_title = full_title
-    start_bracket = stripped_title.find('[')
-    if start_bracket > -1:
-        end_bracket = stripped_title.find(']', start_bracket)
-        if end_bracket > -1:
-            stripped_title = stripped_title[0:start_bracket].strip()
-    featuring = stripped_title.lower().find('feat.')
-    if featuring > -1:
-        stripped_title = stripped_title[0:featuring].strip()
-    if strip_parens:
-        left_paren = stripped_title.find('(')
-        if left_paren > -1:
-            right_paren = stripped_title.find(')', left_paren)
-            if right_paren > -1:
-                stripped_title = stripped_title[0:left_paren].strip()
-    return stripped_title
 
 @print_elapsed_time
 def startImportPlex():
@@ -214,3 +189,25 @@ def startImportPlex():
                     if len(removeTracks) > 0:
                         pplaylist.removeItems(removeTracks)
                         print('Removed '+ str(len(newTracks)) +' tracks to plex playlist '+ playlist.title)
+
+
+def forkImportPlex():
+    # Start foo as a process
+    print(strftime("%Y-%m-%d %H:%M:%S", gmtime())+" Starting startImportPlex")
+    p = multiprocessing.Process(target=startImportPlex)
+    p.start()
+
+    p.join()
+
+    # If thread is active
+    if p.is_alive():
+        print(strftime("%Y-%m-%d %H:%M:%S", gmtime())+" ImportPlex are running... let's kill it...")
+        # Terminate foo
+        p.terminate()
+        # Cleanup
+        p.join()
+
+if __name__ == '__main__':
+    forkImportPlex()
+    #main()
+    #mainSchedule()
